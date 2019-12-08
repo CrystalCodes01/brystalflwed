906d69f451b9804939d331da4b2c465607ee0321
Crystal Maria <crystalcodes.0@gmail.com>
2019-12-07 19:51:17 -0500

    add a messy iframe why not

--- brystalwed/src/App.js
+++ brystalwed/src/App.js
@@ -11,6 +11,15 @@ function App() {
       <div>
         <img src="./brystal.jpg" className="App-logo" alt="logo" />
       </div>
+
+      <iframe
+        title="View of Resort"
+        src="https://www.google.com/maps/embed?pb=!4v1575765343507!6m8!1m7!1sBMG48F_7bnCSLWxAIYdBHw!2m2!1d26.93430390929882!2d-80.06897447890317!3f303.0576474058768!4f0.6153406436869773!5f0.7820865974627469"
+        width="600"
+        height="450"
+        frameborder="0"
+        allowfullscreen=""
+      />
     </div>
     // <a
     //   className="App-link"
