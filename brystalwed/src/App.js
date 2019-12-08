afb358ebb32ac766e60368cfc6f0c308f2d06824
Crystal Maria <crystalcodes.0@gmail.com>
2019-12-07 23:11:58 -0500

    CDNs for now fo rgrid and added react router dom

--- brystalwed/src/App.js
+++ brystalwed/src/App.js
@@ -8,18 +8,29 @@ function App() {
         <h1>Bryan and Crystals Wedding</h1>
       </header>
 
-      <div>
-        <img src="./brystal.jpg" className="App-logo" alt="logo" />
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <img
+              src="./brystal.jpg"
+              className="img-fluid"
+              alt="Bryan and Crystal"
+            />
+          </div>
+
+          <div class="col">
+            <iframe
+              title="View of Resort"
+              src="https://www.google.com/maps/embed?pb=!4v1575765343507!6m8!1m7!1sBMG48F_7bnCSLWxAIYdBHw!2m2!1d26.93430390929882!2d-80.06897447890317!3f303.0576474058768!4f0.6153406436869773!5f0.7820865974627469"
+              width="600"
+              height="450"
+              frameborder="0"
+              allowfullscreen=""
+            />
+          </div>
+        </div>
       </div>
     </div>
-    // <iframe
-    //   title="View of Resort"
-    //   src="https://www.google.com/maps/embed?pb=!4v1575765343507!6m8!1m7!1sBMG48F_7bnCSLWxAIYdBHw!2m2!1d26.93430390929882!2d-80.06897447890317!3f303.0576474058768!4f0.6153406436869773!5f0.7820865974627469"
-    //   width="600"
-    //   height="450"
-    //   frameborder="0"
-    //   allowfullscreen=""
-    // />
     // <a
     //   className="App-link"
     //   href="https://reactjs.org"
