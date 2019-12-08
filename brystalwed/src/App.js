94f87fc0b49606ea8c75971099d66ba0165c2e96
Crystal Maria <crystalcodes.0@gmail.com>
2019-12-07 19:28:29 -0500

    just some basics

--- brystalwed/src/App.js
+++ brystalwed/src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <header className="header">
-        <h1>
-          Bryan and Crystals Wedding
-        </h1>
+        <h1>Bryan and Crystals Wedding</h1>
       </header>
+
+      <div>
+        <img src="./brystal.jpg" className="App-logo" alt="logo" />
+      </div>
     </div>
-        // <a
-        //   className="App-link"
-        //   href="https://reactjs.org"
-        //   target="_blank"
-        //   rel="noopener noreferrer"
-        // >
-        //   Learn React
-        // </a>
+    // <a
+    //   className="App-link"
+    //   href="https://reactjs.org"
+    //   target="_blank"
+    //   rel="noopener noreferrer"
+    // >
+    //   Learn React
+    // </a>
   );
 }
 
