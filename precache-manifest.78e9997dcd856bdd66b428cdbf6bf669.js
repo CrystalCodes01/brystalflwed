36e7a38bd24627884e110bd75d5862505078d929
Crystal Maria <crystalcodes.0@gmail.com>
2019-12-24 12:21:05 -0500

    Updates

--- precache-manifest.78e9997dcd856bdd66b428cdbf6bf669.js
+++ precache-manifest.78e9997dcd856bdd66b428cdbf6bf669.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0137e21fff03d72f27967b11dfe4f77c",
+    "revision": "8488620b1ab58b3998b4170194b33568",
     "url": "/brystalflwed/index.html"
   },
   {
-    "revision": "992539ecd22b442dd319",
+    "revision": "ae03fa8f2d605b37a68e",
     "url": "/brystalflwed/static/css/main.9ff5918b.chunk.css"
   },
   {
-    "revision": "e1fdbe8e2be21a72af7c",
-    "url": "/brystalflwed/static/js/2.7ceb1518.chunk.js"
+    "revision": "624e3118299bdf1feef8",
+    "url": "/brystalflwed/static/js/2.2696fec2.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/brystalflwed/static/js/2.7ceb1518.chunk.js.LICENSE"
+    "url": "/brystalflwed/static/js/2.2696fec2.chunk.js.LICENSE"
   },
   {
-    "revision": "992539ecd22b442dd319",
-    "url": "/brystalflwed/static/js/main.cb4b0583.chunk.js"
+    "revision": "ae03fa8f2d605b37a68e",
+    "url": "/brystalflwed/static/js/main.e073ed3a.chunk.js"
   },
   {
     "revision": "5a282ca1d6fdc87bc840",
