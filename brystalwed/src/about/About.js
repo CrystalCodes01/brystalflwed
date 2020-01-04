0c54f1d4e8c7adc870bf485eca37b52925b3a06c
Crystal Maria <crystalcodes.0@gmail.com>
2020-01-04 00:45:52 -0500

    about us

--- brystalwed/src/about/About.js
+++ brystalwed/src/about/About.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Banner from "../banner/Banner";
 
 function About() {
   return (
@@ -13,36 +12,43 @@ function About() {
         -
       </p>
       <hr className="my-4" />
-      <section className="container-fluid col-12 col-md-8 pl-md-3 col-lg-6 text-left">
-        <p>
-          This particular love story began in South Florida in early 2018.
+      <section className="container-fluid col-12 col-md-8 pl-md-3 col-lg-8">
+        <p className="larger-1x">
+          <span className="larger-2x">
+            This particular love story began in South Florida in early 2018....
+          </span>
           <br />
-          Bryan was a freshly landed Canadian in the USA trying to make friends
-          and work on his tan.
           <br />
+          Bryan, an exotic Canadian, was freshly landed in the USA trying to
+          make friends and work on his tan.
+          <br /> <br />
           Crystal was a software developer living in Fort Lauderdale, focusing
           mostly on her career and not working on her tan.
+          <br />
           <br />
           Bryan and Crystal met one evening in Boca Raton over dinner. They hit
           it off immediately and never looked back. On date three they were
           already in the ocean diving to see coral, sharks and turtles. Over the
           next two years they grew as a couple and had many more experiences
-          large and small. Crystal finally got a passport and they traveled to
-          South America. Bryan finally got to try fried catfish, and so it went.
-          A few months ago, Bryan took Crystal back to the spot they went diving
-          on their thrid date. He asked her to marry him along the shoreline
-          that they dove many times before. She said yes, after figuring out
-          that no one was actually drowning in the ocean, he only told her that
-          to get her to look away to get on one knee.
+          large and small.
+          <br />
+          <br />
+          Crystal finally got a passport and they traveled to South America.
+          Bryan finally got to try fried catfish, and so it went. A few months
+          ago, Bryan took Crystal back to the spot they went diving on their
+          third date. He asked her to marry him along the shoreline that they
+          dove many times before. She said yes, after figuring out that no one
+          was actually drowning in the ocean, he only told her that to get her
+          to look away so he could get on one knee.
+          <br />
           <br />
           It was all very romantic.
         </p>
-        {
-          // <div className="col-12">
-          //   <Banner />
-          // </div>
-        }
-        <div className="embed-responsive embed-responsive-16by9">
+        <hr />
+        <h4 className="bottom-1x text-center">
+          An oscar nominated film about all of this.
+        </h4>
+        <div className="embed-responsive embed-responsive-16by9 bottom-2x">
           <iframe
             className="embed-responsive-item"
             title="One Summer In Florida"
