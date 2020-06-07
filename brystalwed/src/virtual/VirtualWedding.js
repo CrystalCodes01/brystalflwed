1464d1151753ec5fb10e3780aa19d8f947b2e088
Crystal Maria <crystalcodes.0@gmail.com>
2020-06-07 13:12:43 -0400

    virtual wedding

--- brystalwed/src/virtual/VirtualWedding.js
+++ brystalwed/src/virtual/VirtualWedding.js
@@ -13,19 +13,57 @@ function VirtualWedding() {
       </p>
       <hr className="my-4" />
       <section className="container-fluid col-12 col-md-8 pl-md-3 col-lg-8">
-        <h3 className="text-center bottom-2x larger-2x">
-          ~ More details Coming Soon ~
-        </h3>
         <p className="larger-1x text-center">
           The virtual wedding will be hosted on Zoom, a free application you can
           download and/or use on your computer.
           <br />
-          <br />
           Find out how to do that <a href="https://zoom.us/">here.</a>
-          <br /> <br />
-          This page will be updated with schedule, invite code, fun virtual
-          wedding stuff and more soon :-)
         </p>
+        <h3 className="text-center top-2x">Virtual Wedding Itinerary</h3>
+        <table className="table table-bordered table-responsive-sm table-itinerary">
+          <thead>
+            <tr>
+              <th scope="col" className="medium-expand">
+                Date
+              </th>
+              <th scope="col">Virtual Wedding Schedule</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Saturday June 20th</th>
+              <td>
+                <b>Wedding Day!</b>
+                <br />
+                <br />
+                <b>6:00pm - 6:40pm</b> - Begin Zoom session. Open and casual for
+                chatting, signing virtual wedding book, and other online wedding
+                related activities.
+                <br />
+                <br />
+                <b>6:45pm - 7:15pm</b> - Wedding Ceremony. Coordinator will mute
+                all noise except the ceremony to allow for everyone to hear.
+                <br />
+                <br />
+                <b>7:15pm - 7:30pm</b> - Bride and Groom greet and talk to
+                guests on Zoom.
+                <br />
+                <br />
+                <b>7:30pm - 8pm</b> - Zoom intermission.  Participants can stay
+                online and chat or log off.  Camera will be turned off during
+                this time
+                <br />
+                <br />
+                <b>8pm - 8:30pm</b> - Speeches will take place on zoom - come if
+                you like!
+                <br />
+                <br />
+                <b>8:30pm</b> - End zoom session - Say goodbye to remaining
+                guests.
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </section>
     </div>
   );
